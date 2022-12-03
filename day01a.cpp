da0172fda2c4898455827978944dffdfdb3a2b47
tysheng123 <86645673+tysheng123@users.noreply.github.com>
2022-12-03 15:45:34 +0800

    Add files via upload

---
+++ day01a.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main() {
+
+  string myText; 
+  int myCal;
+  int totalCal = 0;
+  int maxCal = 0;
+  
+  ifstream numFile("aos22day1.txt"); 
+  
+  while(getline(numFile, myText)){
+    //cout << "myText: " << myText; 
+    if(myText != ""){ // was "\n"
+      myCal = stoi(myText);
+      totalCal += myCal;
+      
+    }else{
+      if(totalCal > maxCal){
+        maxCal = totalCal;
+      }
+      totalCal = 0;
+      
+    }
+  
+  }
+  cout << maxCal << endl;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
